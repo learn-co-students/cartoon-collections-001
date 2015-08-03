403b6e6a40251bee6a40d54f2fe838fd040a682b
ruth.mesfun <ruth.mesfun@flatironschool.com>
2015-08-03 15:34:49 -0400

    All Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf,i|
+    puts "#{i+1}. #{dwarf}" 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(words)
+  long_words = []
+  words.find do |word|
+   if word.length > 4
+      long_words << word
+    end
+  end
+  if long_words.empty?
+    false
+  else
+    true
+  end
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = []
+  ingredients.each do |ingredient|
+    if cheese_types.include?(ingredient)
+      cheese << ingredient
+    end
+  end
+  if cheese.empty? 
+    nil
+  else
+    cheese[0]
+  end
 end
